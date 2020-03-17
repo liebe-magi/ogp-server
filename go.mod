2ef242cb81c22c15d4a7d10b3fff7ce8e87b0797
reeve0930 <reeve0930@gmail.com>
2020-03-18 00:20:47 +0900

    add ogp parse server

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/reeve0930/ogp-server
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/otiai10/opengraph v1.1.1
+)
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/otiai10/opengraph"
+)
+
+func ogpParser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		url := ctx.Query("url")
+		og, err := opengraph.Fetch(url)
+		if err != nil {
+			log.Println(err)
+            ctx.JSON(http.StatusInternalServerError, nil)
+		} else {
+			ctx.JSON(http.StatusOK, og)
+		}
+	}
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ogp", ogpParser())
+	router.Run(":30002")
+}
